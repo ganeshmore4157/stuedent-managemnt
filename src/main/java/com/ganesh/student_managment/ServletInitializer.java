86d8d04b55277f86211d175617147d58ea5abfc0
Ganesh More <150339504+ganeshmore4157@users.noreply.github.com>
2025-08-20 14:35:15 +0530

    Update ServletInitializer.java
    Changes

--- src/main/java/com/ganesh/student_managment/ServletInitializer.java
+++ src/main/java/com/ganesh/student_managment/ServletInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
      protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
